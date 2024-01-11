a2c86bac0fda0481d2dbf3b2885d8bc007342ade
RASHMIKUMAR DHIYAD <fintechlabceo@outlook.com>
2024-01-11 15:50:53 +0530

    Initial commit

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DxBlazorCRUD.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DxBlazorCRUD.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Data/EmployeeService.cs
@@ -0,0 +1,47 @@
+﻿using DxBlazorCRUD.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DxBlazorCRUD.Data
+{
+    public class EmployeeService
+    {
+        private readonly ApplicationDbContext _applicationDbcontext;
+        public EmployeeService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbcontext = applicationDbContext;
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllEmployeesAsync()
+        {
+            return await _applicationDbcontext.Employees.ToListAsync();
+        }
+
+        public async Task<bool> AddNewEmployee(Employee employee)
+        {
+            await _applicationDbcontext.Employees.AddAsync(employee);
+            await _applicationDbcontext.SaveChangesAsync();
+            return true;
+        }
+        public async Task<Employee> GetEmployeeById(int id)
+        {
+            Employee employee = await _applicationDbcontext.Employees.Where(x => x.Id == id).FirstOrDefaultAsync();
+            return employee;
+        }
+
+        public async Task<bool> UpdateEmployeeDetails(Employee employee)
+        {
+            _applicationDbcontext.Employees.Update(employee);
+            await _applicationDbcontext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> DeleteEmployee(Employee employee)
+        {
+            _applicationDbcontext.Employees.Remove(employee);
+            await _applicationDbcontext.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
